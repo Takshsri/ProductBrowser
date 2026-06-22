aac275e4e8d7f8529c4e6e3609355959c570a0e9
Takshsri <aymar131221@gmail.com>
2026-06-22 16:39:02 +0530

    Fix router registration

--- app/main.py
+++ app/main.py
@@ -1,13 +1,13 @@
 from fastapi import FastAPI
-from app.routes.products import router
-from app.routes.ui import router
+from app.routes.products import router as products_router
+from app.routes.ui import router as ui_router
 app = FastAPI(
     title="Product Browser API"
 )
-app.include_router(router)
-app.include_router(router)
-@app.get("/")
-def home():
+app.include_router(products_router)
+app.include_router(ui_router)
+@app.get("/health")
+def health():
     return {
         "message":"API Running successfully"
     }
